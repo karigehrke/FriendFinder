0e520adb30b8f4950de768e58aba306e46199ffc
Vesper <no-reply>
2019-01-05 17:16:45 -0500

    working code so far

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,19 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
+
+    app.post("/api/friends", function (req, res) {
+        friends.push(req.body);
+    });
+
+    app.post("/api/clear", function (req, res) {
+        friends.length = [];
+        res.json({
+            ok: true
+        });
+    });
+
 }
-
-app.post("api/friends", function(req, res) {
-    friends.push(req.body);
-})
-
-app.post("/api/clear", function(req, res) {
-    friends.length = [];
-    res.json({ ok: true});
-})
